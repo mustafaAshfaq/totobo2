113932bef7d07116e8e3a214b78c01ca4eb9332d
mustafaashfaq <mustafa.ash@gmail.com>
2021-03-13 10:24:39 +0530

    update section 1,2,4,9

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,18 @@
 module.exports.API_SECRET = 'some_secret_string';
 module.exports.JWT_ALGORITHM = 'HS512';
 module.exports.TIMEOUT_DURATION = '1h';
+var apiUrl='https://m.showbahis110.com/apiv3'
+if (process.env.NODE_ENV === 'production') {
 
-module.exports.BASEURL='https://m.showbahis103.com/apiv3';
+    if(process.env.API_URI && process.env.API_URI!=='')
+        apiUrl = process.env.API_URI;
+    //split dburi to get user name and password for db
+    //mongoose.connect(dbURI, {
+    //    auth: {
+    //        user: 'birotydb',
+    //        password: 'GvNlnWXftt5GubOdXDQ3vKf4AyWUG8ZTlFtrNB78tbJi7W4iHxmZTMFCR5gZX23sQpoLE1XYhBSP0Ns73NBhLA=='
+    //    }
+    //});
+
+}
+module.exports.BASEURL=apiUrl;
